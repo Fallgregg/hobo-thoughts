83dfd8403c610aa91cf6e08b9fab64f76ee7956a
Dmitriy <ba.dm.ig.bdi@gmail.com>
2021-03-11 01:39:00 +0200

    dependencies, devDep added for the backend + koa index.js edited

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,32 @@
+
 const Koa = require("koa");
+const BodyParser = require("koa-bodyparser");
+const Router = require("koa-router");
+const Logger = require("koa-logger");
+const serve = require("koa-static");
+const mount = require("koa-mount");
+const cors = require('koa-cors');
+const HttpStatus = require("http-status");
+
 const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.body = "Hello World";
+const PORT = process.env.PORT || 3000;
+
+app.use(BodyParser());
+app.use(Logger());
+app.use(cors());
+
+const router = new Router();
+
+router.get("/general",async (ctx,next)=>{
+  const books = ["post1", "post2", "post3"];
+  ctx.status = HttpStatus.OK;
+  ctx.body = books;
+  await next();
 });
 
-app.listen(3000);
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(PORT, function () {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/", PORT, PORT);
+});
