40fe10bdc0cfdec9396298962139cd8750e57bf8
Dmitriy <ba.dm.ig.bdi@gmail.com>
2021-04-28 21:42:00 +0300

    partial routing + static routing tests added to server

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,15 +18,87 @@ app.use(cors());
 
 const router = new Router();
 
-router.get("/general",async (ctx,next)=>{
-  const books = ["post1", "post2", "post3"];
+router.get("/",async (ctx,next)=>{
+  let bodyContent = "";
+  for (let i = 0; i < posts.length; i++) {
+    bodyContent += `
+    <h3>${posts[i].title}</h3>
+    <p>${posts[i].text}</p>
+    <span>${posts[i].likes}</span>
+    <br><br>
+  `;
+  }
+
   ctx.status = HttpStatus.OK;
-  ctx.body = books;
+  ctx.body = bodyContent;
+
+  delete bodyContent;
+  
+  await next();
+});
+
+router.get("/profile",async (ctx,next)=>{
+  let bodyContent = `
+    <h1>${profile.login}</h1>
+    <div>
+      <span>Following: ${profile.following}</span>
+      <span>Followers: ${profile.followers}</span>
+    </div>
+    <br><br>
+  `;
+  for (let i = 0; i < profile.posts.length; i++) {
+    bodyContent += `
+    <h3>${profile.posts[i].title}</h3>
+    <p>${profile.posts[i].text}</p>
+    <span>${profile.posts[i].date}</span>
+    <br><br>
+  `;
+  }
+
+  ctx.status = HttpStatus.OK;
+  ctx.body = bodyContent;
+
+  delete bodyContent;
+
   await next();
 });
 
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(PORT, function () {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/", PORT, PORT);
+    console.log("==> To see an app visit http://localhost:%s/", PORT);
 });
+
+
+const posts = [
+  {
+    title: "Post Title 1",
+    text: "Limia porbeagle shark morwong dragonfish grunion southern grayling wolffish. Graveldiver rohu smoothtongue tommy ruff, wallago flathead, Pacific cod Canthigaster rostrata panga, Reef triggerfish crestfish whiting.",
+    likes: 100,
+  },{
+    title: "Post Title 2",
+    text: "Limia porbeagle shark morwong dragonfish grunion southern grayling wolffish. Graveldiver rohu smoothtongue tommy ruff, wallago flathead, Pacific cod Canthigaster rostrata panga, Reef triggerfish crestfish whiting.",
+    likes: 101,
+  },{
+    title: "Post Title 3",
+    text: "Limia porbeagle shark morwong dragonfish grunion southern grayling wolffish. Graveldiver rohu smoothtongue tommy ruff, wallago flathead, Pacific cod Canthigaster rostrata panga, Reef triggerfish crestfish whiting.",
+    likes: 102,
+  },
+];
+
+const profile = {
+  login: "Fallgregg",
+  following: 120,
+  followers: 19,
+  posts: [
+    {
+      title: "Post Title 1",
+      text: "Limia porbeagle shark morwong dragonfish grunion southern grayling wolffish. Graveldiver rohu smoothtongue tommy ruff, wallago flathead, Pacific cod Canthigaster rostrata panga, Reef triggerfish crestfish whiting.",
+      date: "12/12/2021",
+    },{
+      title: "Post Title 2",
+      text: "Limia porbeagle shark morwong dragonfish grunion southern grayling wolffish. Graveldiver rohu smoothtongue tommy ruff, wallago flathead, Pacific cod Canthigaster rostrata panga, Reef triggerfish crestfish whiting.",
+      date: "10/12/2021",
+    },
+  ]
+};
