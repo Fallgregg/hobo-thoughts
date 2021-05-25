4e4e5aeceddae5f2bf1209175d169685310402e6
fallgregg <zabilska51@gmail.com>
2021-05-25 16:36:19 +0300

    redux implementation

---
+++ frontend/src/mock_data/data.js
@@ -0,0 +1,46 @@
+// const posts = [
+//     {
+//       user: {
+//         username: "Fallgregg",
+//         avatar: "avatar1.jpg",
+//       },
+//       post: {
+//         title: "That book omg ..",
+//         text: "- Last week I`ve finished reading 'Find me ' by Andre Aciman ...",
+//         date: "20.04.2021",
+//       },
+//     },
+//     {
+//       user: {
+//         username: "Sasuke_Uchiha",
+//         avatar: "avatar2.jpg",
+//       },
+//       post: {
+//         title: "That book omg ..",
+//         text: "It`s okay when it`s in a three way",
+//         date: "20.04.2021",
+//       },
+//     },
+//     {
+//       user: {
+//         username: "Tchalamet",
+//         avatar: "",
+//       },
+//       post: {
+//         title: "Last night",
+//         text: "The golden row",
+//         date: "20.04.2021",
+//       },
+//     },
+//     {
+//       user: {
+//         username: "Fallgregg",
+//         avatar: "avatar3.jpg",
+//       },
+//       post: {
+//         title: "CMBYN",
+//         text: "The best movie, I died",
+//         date: "20.04.2021",
+//       },
+//     },
+//   ];
