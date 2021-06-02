213e20fb3a9ff54609e2bac048c902dd4fc52fac
fallgregg <zabilska51@gmail.com>
2021-06-02 22:29:58 +0300

    fixing conflicts with backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,12 @@ const serve = require("koa-static");
 const mount = require("koa-mount");
 const cors = require('koa-cors');
 const HttpStatus = require("http-status");
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+const app = new Koa();
+
+require('./blocks/db-connect')();	//connect to MongoDB
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,3 +20,8 @@ app.use(BodyParser());
 app.use(Logger());
 app.use(cors());
 
+require('./blocks/routes')(app);
+
+app.listen(PORT, function () {
+    console.log("==> To see an app visit http://localhost:%s/", PORT);
+});
